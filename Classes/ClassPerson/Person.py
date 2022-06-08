5fa6e847a650bd3a864b5fdca188904472cd99e6
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-06-09 02:21:14 +0300

    Create Person.py

---
+++ Classes/ClassPerson/Person.py
@@ -0,0 +1,43 @@
+from cabecalho import cabecalho
+
+
+class Person:
+
+    def __init__(self, name: str, age: int, weight: float, height: float):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+    def geting_old(self):
+        if self.age < 21:
+            self.height += 0.5
+        self.age += 1
+
+    def geting_fat(self, up_weight):
+        self.weight += up_weight
+
+    def losing_weight(self, dow_weight):
+        self.weight -= dow_weight
+
+    def to_grow(self, new_height):
+        self.height += new_height
+
+    def show_person(self):
+        print(f'Name: {self.name}; \nAge: {self.age} years old; \nWeight: {self.weight}kg; \nHeight: {self.height}cm')
+
+
+Inacio = Person('Inacio', 20, 48, 169)
+Inacio.show_person()
+
+cabecalho('GETING OLD')
+Inacio.geting_old()
+Inacio.show_person()
+
+cabecalho('GETING FAT')
+Inacio.geting_fat(2)
+Inacio.show_person()
+
+cabecalho('LOSING WEIGHT')
+Inacio.losing_weight(1)
+Inacio.show_person()
