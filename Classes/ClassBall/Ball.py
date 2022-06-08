7e4222c468718451d49857131807edf70486aa40
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-06-09 02:14:08 +0300

    Create Ball.py

---
+++ Classes/ClassBall/Ball.py
@@ -0,0 +1,23 @@
+# Classe Bola: Crie uma classe que modele uma bola:
+# Atributos: Cor, circunferência, material
+# Métodos: trocaCor e mostraCor
+
+class Ball:
+
+    def __init__(self, color, circunference, material):
+        self.color = color
+        self.circunference = circunference
+        self.material = material
+
+    def change_color(self, new_color):
+        self.color = new_color
+
+    def show_color(self):
+        print(self.color, self.circunference, self.material)
+
+
+ball1 = Ball('Blue', 5.5, 'Saco')
+ball1.show_color()
+
+ball1.change_color('Red')
+ball1.show_color()
